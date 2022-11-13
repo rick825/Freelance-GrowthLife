eb1b36f9c73642f000099ef3c00e44e7a1e0a9c8
Akash Das <69415418+rick825@users.noreply.github.com>
2022-11-13 21:58:25 +0530

    Add files via upload

---
+++ main/Employer/profile.js
@@ -0,0 +1,14 @@
+const ps = () =>{
+
+
+const ham = document.querySelector('.navmenu');
+const profile = document.querySelector('.profile-i');
+// const profileshow = document.querySelector('.profileshow');
+
+ham.addEventListener('click',()=>{
+console.log("akash");
+ profile.classList.add('profileshow');
+});
+}
+
+ps();
